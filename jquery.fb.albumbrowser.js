75f0746bb50e5ee80e26982a302904fa596298df
cak-opiq <a_rofiq@yahoo.com>
2016-10-14 09:37:08 +0700

    post

--- jquery.fb.albumbrowser.js
+++ jquery.fb.albumbrowser.js
@@ -743,7 +743,7 @@
 							
 							previewStock.unbind("click");
 							previewStock.click(function() {
-								$.post('http://pojokbayi.com/album/getstock.php',{fbid:"data-id"}, function(result){
+								$.post('http://pojokbayi.com/album/getstock.php',{fbid:$('.fb-preview-img').attr("data-id")}, function(result){
 									if (result){
 										console.log(result.total);
 										for(i=0; i<result.rows.length; i++){
